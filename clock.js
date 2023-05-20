145de8c1c3733b1e27f0b37865c08b58b1252b3b
ROSHAN REDDY RACHAMALLA <roshanrithik279@gmail.com>
2023-05-20 10:40:52 +0530

    few changes

--- clock.js
+++ clock.js
@@ -25,17 +25,14 @@ setInterval(() => {
 
     }
     if(hours<10){
-        
         hours= "0" + hours;
 
     }
     if(minutes<10){
-        
        minutes= "0" + minutes;
 
     }
     if(seconds<10){
-        
         seconds= "0" + seconds;
 
     }
